ec915240ed5f00849765a331e14784c151257e37
Enrico Paganin <enrico.paganin@mail.com>
2017-01-02 11:50:03 +0100

    Improve polybar launcher and move termite conf

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-killall polybar 
+killall -q polybar 
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
 polybar top -q &
 polybar bottom -q &
